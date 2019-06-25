519d6a5eb39eaa404402bdc63de47cae403c6cd1
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2019-06-25 18:40:44 +0200

    Update and rename neurone2.cpp to neurone2.h

--- neurone2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-class Neurone2 {
-  public : std :: string inizializzazioneNeurone2 = "\nInizializzazione del secondo neurone ...\n";
-  public: void funzioneNeurone2(){
-    std :: cout <<inizializzazioneNeurone2;
-  }
-};
