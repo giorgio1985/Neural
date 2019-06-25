ae158c6c0d46503a43568e6c917df4c0a4eba0ad
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2019-06-25 18:37:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
-#include <iostream>
-#include <string>
+# include  <iostream>
+# include  <string>
+# include  "neurone1.h"
+# include  "neurone2.h"
+# include  "neurone3.cpp"
 
-#include "neurone1.cpp"
-#include "neurone2.cpp"
-#include "neurone3.cpp"
-
-int main() {
-
+int  main () {
+//L'inizializzazione serve per vedere se fila tutto liscio prima della fase vera e propria del programma ... 
+int a1, a2, b1, b2;
   Neurone1 n1;
-  n1.funzioneNeurone1();
+  n1. funzioneNeurone1 ();
 
   Neurone2 n2;
-  n2.funzioneNeurone2();
+  n2. funzioneNeurone2 ();
 
   Neurone3 n3;
-  n3.funzioneNeurone3();
+  n3. funzioneNeurone3 ();
 
-  
+//Inizio della parte di inserimento e di calcolo del programma ...
 
-  return 0;
+  n1.funzionePrimoAnnuncioNeurone1();
+  n1.funzionePrimoValoreNeurone1(a1);
+  n1.funzioneSecondoAnnuncioNeurone1();
+  n1.funzioneSecondoValoreNeurone1(a2);
+  std :: cout << n1.funzioneCalcoloNeurone1();
+
+  n2.funzionePrimoAnnuncioNeurone2();
+  n2.funzionePrimoValoreNeurone2(b1);
+  n2.funzioneSecondoAnnuncioNeurone2();
+  n2.funzioneSecondoValoreNeurone2(b2);
+  std :: cout << n2.funzioneCalcoloNeurone2();
+
+
+
+
+
+  return  0;
 }
