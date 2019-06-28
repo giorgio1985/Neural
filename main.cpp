9cab4ac94fc22c4229e54604c44ec5abb3aae118
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2019-06-28 18:16:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,106 @@
-# include  <iostream>
-# include  <string>
-# include  "neurone1.h"
-# include  "neurone2.h"
-# include  "neurone3.cpp"
+#include <iostream>
+#include <string>
+#include <stdlib.h>
 
-int  main () {
-//L'inizializzazione serve per vedere se fila tutto liscio prima della fase vera e propria del programma ... 
-int a1, a2, b1, b2;
+ class Neurone1 {
+  //public: int valore1, valore2;
+  public: void inizializzazioneN1() {
+    std :: cout <<"\n neurone 1 inizializzato!\n ";
+  }
+
+  public: void inserisciPrimoValoreN1() {
+    std :: cout<<"\n inserisci il primo valore del primo nodo\n";
+  }
+
+  public: int primoValoreN1(int valore1) {
+    return valore1;
+
+  }
+
+  public: void inserisciSecondoValoreN1() {
+    std :: cout<<"\n inserisci il secondo valore del primo nodo\n";
+  }
+
+  public: int secondoValoreN1(int valore2) {
+    return valore2;
+
+  }
+
+};
+
+class Neurone2 {
+
+  public: void inizializzazioneN2() {
+    std :: cout <<"\n neurone 2 inizializzato!\n";
+  }
+  
+  public: void inserisciPrimoValoreN2() {//
+    std :: cout <<"\n inserisci il primo valore del secondo nodo \n";
+  }
+
+  public: int primoValoreN2(int valore1) {
+    return valore1;
+
+  }
+
+  public: void inserisciSecondoValoreN2() {
+    std :: cout <<"\n inserisci il secondo valore del secondo nodo\n";
+  }
+
+  public: int secondoValoreN2(int valore2) {
+    return valore2;
+
+  }
+
+};
+
+class Neurone3: public Neurone1, public Neurone2 {
+  public: void inizializzazioneN3() {
+    std :: cout <<"\n neurone 3 inizializzato!\n";
+  }
+
+  public: int calcoloAttivazioneN3(int valore1, int valore2) {
+    return valore1 + valore2;
+
+  }
+
+  
+};
+
+int main () {
+
+  int primo1, primo2, uscitaNodo1;
+  int secondo1, secondo2, uscitaNodo2;
+  int attivazione;
+
   Neurone1 n1;
-  n1. funzioneNeurone1 ();
-
   Neurone2 n2;
-  n2. funzioneNeurone2 ();
-
   Neurone3 n3;
-  n3. funzioneNeurone3 ();
 
-//Inizio della parte di inserimento e di calcolo del programma ...
+  n1.inizializzazioneN1();
+  n2.inizializzazioneN2();
+  n3.inizializzazioneN3();
 
-  n1.funzionePrimoAnnuncioNeurone1();
-  n1.funzionePrimoValoreNeurone1(a1);
-  n1.funzioneSecondoAnnuncioNeurone1();
-  n1.funzioneSecondoValoreNeurone1(a2);
-  std :: cout << n1.funzioneCalcoloNeurone1();
+  n1.inserisciPrimoValoreN1();
+  std :: cin >> primo1;
+  n1.primoValoreN1(primo1);
+  n1.inserisciSecondoValoreN1();
+  std :: cin >> primo2;
+  n1.secondoValoreN1(primo2); 
+  uscitaNodo1 = n1.primoValoreN1(primo1)+n1.secondoValoreN1(primo2);
+  std :: cout <<uscitaNodo1;
 
-  n2.funzionePrimoAnnuncioNeurone2();
-  n2.funzionePrimoValoreNeurone2(b1);
-  n2.funzioneSecondoAnnuncioNeurone2();
-  n2.funzioneSecondoValoreNeurone2(b2);
-  std :: cout << n2.funzioneCalcoloNeurone2();
+  n2.inserisciPrimoValoreN2();
+  std :: cin >> secondo1;
+  n2.primoValoreN2(secondo1);
+  n2.inserisciSecondoValoreN2();
+  std :: cin >> secondo2;
+  n2.secondoValoreN2(secondo2);
+  uscitaNodo2 = n2.primoValoreN2(secondo1)+n2.secondoValoreN2(secondo2);
+  std :: cout <<uscitaNodo2;
+  
+  attivazione = n3.calcoloAttivazioneN3(uscitaNodo1, uscitaNodo2);
+  std :: cout <<"\n"<<attivazione;
 
-
-
-
-
-  return  0;
+ return 0;
 }
