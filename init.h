dcda7a2a57e9bb5eb36fc3c9adca4ba730700fed
giorgio1985 <g.adonoo@yahoo.it>
2020-03-23 23:46:04 +0100

    init file creation

---
+++ init.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+using std::string;
+using namespace :: std;
+
+
+class init {
+   public: string guide0 = "\n\n************      Instructions      ***********\n\n";
+   public: string guide1 = "1) Tensorflow c++ installation guide --> https://www.eiffel.org/blog/javier/2019/10/wrapping-tensorflow-c-api-wrapc\n";
+   public: string guide2 = "2) click above in the right in your browser environment, the low arrow\n";
+   public: string guide3 = "3) Move .gz file in Documents folder and extract here\n";
+   public: string guide4 = "4) You can find your file here -->  ../usr/local/lib directory \n";
+   public: string guide5 = "5) Write here in ../usr/local/lib this -- > sudo ldconfig <-- for the linker\n";
+   public: string guide6 = "\n\n -----          Have a nice code!      ------   : - ) \n\n";
+
+
+   public: void showInit() {
+
+   cout <<guide0;
+   cout <<guide1;
+   cout <<guide2;
+   cout <<guide3;
+   cout <<guide4;
+   cout <<guide5;
+   cout <<guide6;
+}
+
+};
--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
-#include <tensorflow/c/c_api.h>
+//#include <tensorflow/c/c_api.h>
 #include "tensor.h"
+#include "init.h"
 
 
 using namespace :: std;
@@ -12,26 +13,12 @@ using std::string;
   int main()
   {
 
-   //string tensor :: showSayHello();
+    init * i = new init();
     tensor *t = new tensor();
+
+    i->showInit();
     cout<<(t->showSayHello());
 
-
-   string guide0 = "\n\n************      Instructions      ***********\n\n";
-   string guide1 = "1) Tensorflow c++ installation guide --> https://www.eiffel.org/blog/javier/2019/10/wrapping-tensorflow-c-api-wrapc\n";
-   string guide2 = "2) click above in the right in your browser environment, the low arrow\n";
-   string guide3 = "3) Move .gz file in Documents folder and extract here\n";
-   string guide4 = "4) You can find your file here -->  ../usr/local/lib directory \n";
-   string guide5 = "5) Write here in ../usr/local/lib this -- > sudo ldconfig <-- for the linker\n";
-   string guide6 = "\n\n -----          Have a nice code!      ------   : - ) \n\n";
-
-   cout <<guide0;
-   cout <<guide1;
-   cout <<guide2;
-   cout <<guide3;
-   cout <<guide4;
-   cout <<guide5;
-   cout <<guide6;
 
     return 0;
   }
